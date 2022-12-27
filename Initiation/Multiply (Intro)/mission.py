b4e7727bd00fd771f5e1b02c9056cd0382039ae0
Peresvet <t9221611197@gmail.com>
2022-12-27 14:24:51 +0500

    Initial commit

---
+++ Initiation/Multiply (Intro)/mission.py
@@ -0,0 +1,17 @@
+def mult_two(a, b):
+    # your code here
+    return None
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(mult_two(3, 2))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert mult_two(3, 2) == 6
+    assert mult_two(1, 0) == 0
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+def mult_two(a: int, b: int) -> int:
+    return a * b
